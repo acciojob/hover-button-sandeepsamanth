d09b36fb53329c203e7a0dd0dd0ba3a5307e0ce3
sandeepsamanth <98205088+sandeepsamanth@users.noreply.github.com>
2023-01-13 14:43:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+document.getElementById("btn").onmouseover = function() {
+  this.style.backgroundColor = "yellow";
+  this.style.color = "black";
+  this.style.borderRadius = "30px";
+}
+document.getElementById("btn").onmouseout = function() {
+  this.style.backgroundColor = "black";
+  this.style.color = "white";
+  this.style.borderRadius = "5px";
+}
